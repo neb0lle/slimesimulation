e08a563a0a06031ae7d30f2e32534fac84ddeb77
NevilleJS <nevillejs24@gmail.com>
2021-06-29 09:20:40 +0530

    setup agent initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <raylib.h>
 #include <iostream>
+#include <vector>
 #include <cmath>
 using namespace std;
 
-#define PI 3.1415926535897932384626433832795
-
+const int RES = 500;
 struct Agent{
     Vector2 position;
     Vector2 angle;
@@ -21,24 +21,37 @@ uint hash_func(uint state){
     // max: 4294967295
 }
 
-void Update(Agent &agent){ 
+float hash_scale(uint state){
+    return state / 4294967295.0;
+}
+
+vector<Agent> agent_list;
+void agent_init(){
+    for(int i=(RES/2)-5;i<RES/2+5;++i){
+        for(int j=(RES/2)-5;j<RES/2+5;++j){
+            float randangle =hash_scale(hash_func(i*RES+j))*360*M_PI/180;
+            agent_list.push_back(Agent{{(float)i,(float)j},{cos(randangle),sin(randangle)}});
+        }
+    }
+}
+void agent_update(Agent &agent){ 
     agent.position.x += agent.angle.x;
     agent.position.y += agent.angle.y;
+    if(agent.position.x<0 || agent.position.x>RES) agent.angle.x *= -1;
+    if(agent.position.y<0 || agent.position.y>RES) agent.angle.y *= -1;
     DrawPixelV(agent.position,WHITE);
 }
 int main(){
     SetConfigFlags(FLAG_MSAA_4X_HINT);
-    InitWindow(250,250,"slime");
+    InitWindow(RES,RES,"sed");
     SetTargetFPS(60);
     ClearBackground(BLACK);
-    for(int i=0;i<250;++i){
-       for(int j=0;j<250;++j){
-            Color temp = {255,255,255,(float)hash_func(i*250+j)/4294967295.0*255};
-            DrawPixel(i,j,temp);
-         }
-    }
+    agent_init();
     while(!WindowShouldClose()){
         BeginDrawing();
+        for(int k=0;k<agent_list.size();++k){
+            agent_update(agent_list[k]);
+        }
         EndDrawing();
     }
 }
