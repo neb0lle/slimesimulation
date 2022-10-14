734adc8d1993057dcab639803ae94259ecfc16e3
NevilleJS <69287021+s3dman@users.noreply.github.com>
2022-10-14 19:35:59 +0530

    bruh

--- src/main.cpp
+++ src/main.cpp
@@ -7,79 +7,95 @@ using namespace std;
 
 const int RES=1000;
 
-struct Agent{
+struct Agent {
     Vector2 position;
     Vector2 angle;
-	pair<bool,bool> is_oob;
+    // pair<bool,bool> is_oob;
 };
 
 vector<Agent> agent_list;
-
 extern Color *pixels; // import pixel array from render.cpp
 
 
 void Sense(Agent &agent);
 
-void UpdateData(Agent &agent){
-    agent.position.x += agent.angle.x; 
-    agent.position.y += agent.angle.y; 
+void UpdateData(Agent &agent) {
+    agent.position.x += agent.angle.x;
+    agent.position.y += agent.angle.y;
 
-	if(agent.position.x<0 || agent.position.x>=RES){
-		if(agent.is_oob.first == false){
-			agent.angle.x *= -1;
-			agent.is_oob.first = true;
-		}
-	}
-	else agent.is_oob.first = false;
-	if(agent.position.y<0 || agent.position.y>=RES){
-		if(agent.is_oob.second == false){
-			agent.angle.y *= -1;
-			agent.is_oob.second = true;
-		}
-	}
-	else agent.is_oob.second = false;
-	for(int k=-1; k<2; ++k){
-		TexPixDraw(agent.position.x+k,agent.position.y,WHITE);
-		TexPixDraw(agent.position.x,agent.position.y+k,WHITE);
-	}
-	Sense(agent);
+    // primitive method (not considering out of bounds)
+    if(agent.position.x<=0) {
+        agent.angle.x *= -1;
+        agent.position.x = 0;
+    }
+    else if (agent.position.x>=RES) {
+        agent.angle.x *= -1;
+        agent.position.x = RES;
+    }
+    if(agent.position.x<=0) {
+        agent.angle.x *= -1;
+        agent.position.x = 0;
+    }
+    else if (agent.position.x>=RES) {
+        agent.angle.x *= -1;
+        agent.position.x = RES;
+    }
+
+    // more complex method (considering out of bounds)
+    // if(agent.position.x<0 || agent.position.x>=RES) {
+    //     if(agent.is_oob.first == false) {
+    //         agent.angle.x *= -1;
+    //         agent.is_oob.first = true;
+    //     }
+    // }
+    // else agent.is_oob.first = false;
+    // if(agent.position.y<0 || agent.position.y>=RES) {
+    //     if(agent.is_oob.second == false) {
+    //         agent.angle.y *= -1;
+    //         agent.is_oob.second = true;
+    //     }
+    // }
+    // else agent.is_oob.second = false;
+    for(int k=-1; k<2; ++k) {
+        TexPixDraw(agent.position.x+k,agent.position.y,WHITE);
+        TexPixDraw(agent.position.x,agent.position.y+k,WHITE);
+    }
+    Sense(agent);
 }
 
-void Sense(Agent &agent){
-	int max_intensity = 0;
-	if(agent.angle.x>0 && agent.angle.y>0){
-		for(int i=0; i<3; ++i){
-			for(int j=0; j<3; ++j){
-			}
-		}
-	}
+void Sense(Agent &agent) {
+    int max_intensity = 0;
+    if(agent.angle.x>0 && agent.angle.y>0) {
+        for(int i=0; i<3; ++i) {
+            for(int j=0; j<3; ++j) {
+            }
+        }
+    }
 }
 
 // Initialize induvigual agents with random angle
-void AgentInit(float x, float y){
+void AgentInit(float x, float y) {
     float randangle =hash_scale(hash_func(y*RES+x))*360*M_PI/180;
-    agent_list.push_back(Agent{{x,y},{cos(randangle),sin(randangle)},{false,false}});
-    
+    agent_list.push_back(Agent{{x,y},{cos(randangle),sin(randangle)}});
+
 }
 
 // Initial bulk generation of agents with random angle
-void RandomAgentGenerator(int x, int rangl=0, int rangr=RES){
-    for(int b=0;b<x;++b){
+void RandomAgentGenerator(int x, int rangl=0, int rangr=RES) {
+    for(int b=0; b<x; ++b) {
         float rango =hash_scale(hash_func(GetRandomValue(rangl,rangr)))*360*M_PI/180;
-		agent_list.push_back(Agent{
-				{
-					static_cast<float>(GetRandomValue(rangl,rangr)),
-					static_cast<float>(GetRandomValue(rangl,rangr))
-				},
-				{cos(rango)*2,sin(rango)*2},
-				{false,false}}
-				); 
+        agent_list.push_back(Agent{
+            {
+                static_cast<float>(GetRandomValue(rangl,rangr)),
+                static_cast<float>(GetRandomValue(rangl,rangr))
+            },
+            {cos(rango)*2,sin(rango)*2}});
     }
 }
 
 int main()
 {
-    SetConfigFlags(FLAG_MSAA_4X_HINT); 
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
     SetTargetFPS(60);
     InitWindow(RES, RES, "sed");
     Image tuxim = {
@@ -95,21 +111,21 @@ int main()
     CLS();
     RandomAgentGenerator(1000,450,550);
 
-    while (!WindowShouldClose()){
+    while (!WindowShouldClose()) {
         // INPUT HANDLING
-        if(GetGestureDetected()==GESTURE_DRAG){
+        if(GetGestureDetected()==GESTURE_DRAG) {
             Vector2 mousePos = GetMousePosition();
             AgentInit(mousePos.x,mousePos.y);
         }
 
         DiffuseTexture();
         BeginDrawing();
-            DrawTexture(tux,0,0,WHITE);
-			for(int k=0;k<agent_list.size();++k){
-				UpdateData(agent_list[k]);
-			}
+        DrawTexture(tux,0,0,WHITE);
+        for(int k=0; k<agent_list.size(); ++k) {
+            UpdateData(agent_list[k]);
+        }
         EndDrawing();
         UpdateTexture(tux,pixels);
     }
-	UnloadTexture(tux);
+    UnloadTexture(tux);
 }
