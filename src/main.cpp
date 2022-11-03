1c11a32ea3c3c30f4c197027bd596c0e88e1894f
NevilleJS <69287021+s3dman@users.noreply.github.com>
2022-11-03 11:13:14 +0530

    rbuh fixed

--- src/main.cpp
+++ src/main.cpp
@@ -21,11 +21,13 @@ extern Color *pixels; // import pixel array from render.cpp
 
 void Sense(Agent &agent);
 
+void DebugData(Agent &agent) {
+    printf("(%f,%f):(%f,%f)\n",agent.position.x,agent.position.y,agent.angle.x,agent.angle.y);
+}
 void UpdateData(Agent &agent,float speed=1) {
     agent.position.x += agent.angle.x*speed;
     agent.position.y += agent.angle.y*speed;
 
-    // primitive method (not considering out of bounds)
     if(agent.position.x<=0) {
         agent.angle.x *= -1;
         agent.position.x = 0;
@@ -47,23 +49,17 @@ void UpdateData(Agent &agent,float speed=1) {
         TexPixDraw(agent.position.x+k,agent.position.y,WHITE);
         TexPixDraw(agent.position.x,agent.position.y+k,WHITE);
     }
-    // Sense(agent);
+    Sense(agent);
 }
 
-// void Sense(Agent &agent) {
-//     int max_intensity = 0;
-//     if(agent.angle.x>0 && agent.angle.y>0) {
-//         for(int i=0; i<3; ++i) {
-//             for(int j=0; j<3; ++j) {
-//             }
-//         }
-//     }
-// }
+void Sense(Agent &agent) {
+    DebugData(agent);
+}
 
 // Initialize induvigual agents with random angle at x,y
 void AgentInit(float x, float y) {
     float randangle = GetRandomValue(0,360);
-    Vector2 agent_angle = {sin(randangle),cos(randangle)};
+    Vector2 agent_angle = {cos(static_cast<float>(randangle*M_PI/180)),sin(static_cast<float>(randangle*M_PI/180))};
     agent_list.push_back(Agent{{x,y},agent_angle});
 }
 
@@ -71,7 +67,8 @@ void AgentInit(float x, float y) {
 void RandomAgentGenerator(int n, int rangl=0, int rangr=RES) {
     for(int b=0; b<n; ++b) {
         float randangle = GetRandomValue(0,360);
-        Vector2 agent_angle = {sin(randangle),cos(randangle)};
+        printf("angle: %f\n",randangle);
+        Vector2 agent_angle = {cos(static_cast<float>(randangle*M_PI/180)),sin(static_cast<float>(randangle*M_PI/180))};
         agent_list.push_back(Agent{
             { static_cast<float>(GetRandomValue(rangl,rangr)), static_cast<float>(GetRandomValue(rangl,rangr)) },
             agent_angle});
@@ -94,7 +91,9 @@ int main()
     UnloadImage(tuxim);
     ClearBackground(BLACK);
     CLS();
-    // RandomAgentGenerator(10000,350,650);
+    // RandomAgentGenerator(1000,350,650);
+    float temp = 90*M_PI/180;
+    agent_list.push_back(Agent{{500,500},{cos(temp),sin(temp)}});
 
     while (!WindowShouldClose()) {
         // INPUT HANDLING
@@ -102,9 +101,6 @@ int main()
             Vector2 mousePos = GetMousePosition();
             AgentInit(mousePos.x,mousePos.y);
         }
-
-        RandomAgentGenerator(10,0,RES);
-
         DiffuseTexture();
         BeginDrawing();
         DrawTexture(tux,0,0,WHITE);
